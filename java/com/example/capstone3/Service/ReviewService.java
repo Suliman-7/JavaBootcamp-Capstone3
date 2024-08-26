46a1b5c70f2ac737f4401b39059a34f93863ef58
Abdulrahman Bamrie <115091048+abdabd12320@users.noreply.github.com>
2024-08-26 11:08:12 +0300

    Add files via upload

---
+++ java/com/example/capstone3/Service/ReviewService.java
@@ -0,0 +1,43 @@
+package com.example.capstone3.Service;
+
+import com.example.capstone3.Model.Review;
+import com.example.capstone3.Repository.ReviewRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewService {
+
+    private final ReviewRepository reviewRepository;
+
+    public List<Review> getReviews()
+    {
+        return reviewRepository.findAll();
+    }
+    public void addReview(Review review)
+    {
+        reviewRepository.save(review);
+    }
+    public void updateReview(Integer id,Review review)
+    {
+        Review r = reviewRepository.findReviewById(id);
+        if(r == null)
+        {
+//            throw new ApiException("");
+        }
+        r.setComment(review.getComment());
+        r.setRate(review.getRate());
+        reviewRepository.save(r);
+    }
+    public void deleteReview(Integer id)
+    {
+        if(reviewRepository.findReviewById(id) == null)
+        {
+            // throw new ApiException("");
+        }
+        reviewRepository.deleteById(id);
+    }
+}
